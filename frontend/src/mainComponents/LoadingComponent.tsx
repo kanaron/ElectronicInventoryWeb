8aa5c3af9c5fc738ece99508500dcb37eb867dd4
Mateusz Godziek <kanaron125@gmail.com>
2025-03-28 12:49:49 +0100

    trigger deploy

--- frontend/src/mainComponents/LoadingComponent.tsx
+++ frontend/src/mainComponents/LoadingComponent.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function LoadingComponent({
   inverted = true,
-  content = "Loading....",
+  content = "Loading...",
 }: Props) {
   return (
     <Dimmer active={true} inverted={inverted}>
